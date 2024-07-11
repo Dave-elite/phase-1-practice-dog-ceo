d2244d575b5153bf17b9e0b909395fde6e07efc6
David Waruhiu Njenga <davenjenga098@gmail.com>
2024-07-11 20:39:41 +0300

    lab complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,69 @@
 console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded',() => {
+
+    const imageContainer = document.getElementById('dog-image-container')
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+    const breedUrl = "https://dog.ceo/api/breeds/list/all";
+    const ul = document.getElementById('dog-breeds')
+    const select = document.getElementById('breed-dropdown')
+    const value = document.getElementsByTagName('value')
+  function loadImages (){
+    fetch( "https://dog.ceo/api/breeds/image/random/4")
+    .then((response) => response.json())
+    .then((data) => {
+        data.message.forEach((imgUrl) =>  {
+            const img = document.createElement('img')
+            img.src = imgUrl
+            imageContainer.appendChild(img)
+            
+            
+            
+            
+
+        })
+        console.log(data)
+    })
+    .catch
+
+}
+loadImages()
+function  loadbreeds (){
+    fetch("https://dog.ceo/api/breeds/list/all")
+    .then(response => response.json())
+    .then(data => {
+        Object.keys(data.message).forEach((breedUrl) => {
+            const li = document.createElement('li')
+            li.innerHTML = breedUrl
+            ul.appendChild(li);
+
+            li.addEventListener('click', (e) =>{
+                li.style.color = 'green'
+
+              
+
+
+            } )
+
+        })
+        console.log(data)
+    })
+    .catch(error => console.error('Error','An error has occurred'))
+}
+loadbreeds();
+
+function filterBreeds(letter){
+    const listItems = ul.getElementsByTagName('li');
+
+    for(const item of listItems){
+        if (letter === "" || item.textContent.startsWith(letter)){
+            item.style.display = "";
+        }else{
+            item.style.display = 'none'
+        }
+    }
+}
+ select.addEventListener('change', (e) => {
+            const selectedLetter = e.target.value;
+            filterBreeds(selectedLetter);
+ });
+})
